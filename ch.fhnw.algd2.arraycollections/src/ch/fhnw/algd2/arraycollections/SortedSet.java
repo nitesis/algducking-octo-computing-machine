638e8ec47be35610bd1a7928c4d245690ac1c33b
nitesis <viviane.bendjus@students.fhnw.ch>
2015-04-07 12:32:06 +0200

    sorted set

--- ch.fhnw.algd2.arraycollections/src/ch/fhnw/algd2/arraycollections/SortedSet.java
+++ ch.fhnw.algd2.arraycollections/src/ch/fhnw/algd2/arraycollections/SortedSet.java
@@ -7,6 +7,7 @@ public class SortedSet<E extends Comparable<? super E>> extends
 		AbstractArrayCollection<E> implements Set<E> {
 	public static final int DEFAULT_CAPACITY = 100;
 	private Object[] data;
+	private int size = 0;
 
 	public SortedSet() {
 		this(DEFAULT_CAPACITY);
@@ -18,17 +19,45 @@ public class SortedSet<E extends Comparable<? super E>> extends
 
 	@Override
 	public boolean add(E e) {
-		throw new UnsupportedOperationException();
+		if (size == data.length) {
+			throw new IllegalStateException("Sorry, wir sind voll");
+		} else {
+		int i = indexOf(e);
+		if (i >= 0) {
+			return false;
+		} else if (e == null){
+			throw new NullPointerException();
+		} else {
+			i = -(i + 1); // insertion point
+			for (int j = size; j != i; j--) {
+				data[j] = data[j-1];
+			}
+			size++;
+			data[i] = e;
+			return true;
+		}
 	}
+	}	
 
 	@Override
 	public boolean remove(Object o) {
-		throw new UnsupportedOperationException();
+		int i = indexOf(o);
+		if (i < 0) {
+			return false;
+		} else {
+			data[i] = null;
+			while (i != size - 1) {
+				data[i] = data[i + 1];
+				i++;
+			}
+		}size--;
+		return true;
 	}
 
 	@Override
 	public boolean contains(Object o) {
-		throw new UnsupportedOperationException();
+		int i = indexOf(o);
+		return i>=0 && data[i].equals(o);
 	}
 
 	@Override
@@ -38,7 +67,25 @@ public class SortedSet<E extends Comparable<? super E>> extends
 
 	@Override
 	public int size() {
-		return 0;
+		return size;
+	}
+	
+	private int indexOf(Object o) {
+		return Arrays.binarySearch(data, 0, size, o);
+//		if (o==null) {
+//			throw new NullPointerException();
+//		}else{
+//		int l = -1;
+//		int h = size;
+//			while (h - l > 1) {
+//				int m = (l + h) / 2;
+//				if (((E)data[m]).compareTo((E)o) <= 0) {
+//					l = m;
+//				}else{
+//					h = m;
+//				}
+//			}return l;
+//		}
 	}
 
 	public static void main(String[] args) {
