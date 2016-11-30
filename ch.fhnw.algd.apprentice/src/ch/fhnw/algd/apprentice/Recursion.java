ba58c7c63ef6525c56602f091d4b7aea9077e3fd
nitesis <viviane.bendjus@students.fhnw.ch>
2016-12-01 00:19:09 +0100

    simple recursive countdown

---
+++ ch.fhnw.algd.apprentice/src/ch/fhnw/algd/apprentice/Recursion.java
@@ -0,0 +1,18 @@
+package ch.fhnw.algd.apprentice;
+
+public class Recursion {
+
+	public static void main(String[] args) {
+		countdown(6);
+
+	}
+	
+	public static void countdown(int number) {
+		if (number > 0) {
+			System.out.println("Die Nummer lautet: " + number);
+			number--;
+			countdown(number);
+		}
+	}
+
+}
