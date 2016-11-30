dc6075089b624a258081785a2548d9dd8451c07e
nitesis <viviane.bendjus@students.fhnw.ch>
2016-12-01 00:46:05 +0100

    fakultaet rekursive

---
+++ ch.fhnw.algd.apprentice/src/ch/fhnw/algd/apprentice/Fakultaet.java
@@ -0,0 +1,19 @@
+package ch.fhnw.algd.apprentice;
+
+public class Fakultaet {
+
+	public static void main(String[] args) {
+		int ergebnis = fakultaetRecursive(3);
+		System.out.println(ergebnis);
+	}
+	
+	public static int fakultaetRecursive(int n) {
+		if (n == 1) {
+			return 1;
+			
+		} else {
+			return n * fakultaetRecursive(n - 1);
+		}
+	}
+
+}
